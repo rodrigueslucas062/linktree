de3b19b44930ee59a84ca4563f511ceb802674c1
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-02-21 23:21:10 -0300

    📈 SpeedInsights and Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import "@/styles/globals.scss";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+      <Analytics />
+    </>
+  );
 }
