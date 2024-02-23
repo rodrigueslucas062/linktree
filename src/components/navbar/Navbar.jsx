c2e08fa0dae63346a22b38dc539f925d3969c930
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-02-23 17:11:31 -0300

    💄 Backdrop filter in Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 
     useEffect(() => {
         const handleScroll = () => {
-            if (window.scrollY < 10) {
+            if (window.scrollY < 30) {
                 setIsMenuVisible(true);
             } else {
                 setIsMenuVisible(false);
@@ -28,7 +28,7 @@ const Navbar = () => {
 
     return (
         <nav className="flex w-full justify-center fixed">
-            <div className={`${isMenuVisible ? 'hidden' : '-ml-4 mx-4 flex w-full lg:w-2/5 border-2 border-zinc-900 bg-gray-50/70 items-center justify-between px-4 py-2 rounded-full'}`}>
+            <div className={`${isMenuVisible ? 'hidden' : '-ml-4 mx-4 flex w-full lg:w-2/5 border-2 border-zinc-900 bg-gray-50/70 items-center justify-between px-4 py-2 rounded-full backdrop-blur-sm'}`}>
                 <Image className="rounded-xl border-2 border-zinc-900" src={Perfil} alt="Lucas Rodrigues" width={50} height={50} />
                 <h5 className="mb-1 text-xl font-semibold text-zinc-800">Lucas Rodrigues</h5>
                 <div className="bg-zinc-200 hover:bg-zinc-400 hover:text-zinc-200 p-2 rounded-full">
