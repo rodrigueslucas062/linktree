b898855877edcf58b458e3a364e34ee4034b3167
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-02-23 16:58:19 -0300

    🎨Text improvements and code simplifications

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -5,15 +5,13 @@ import LinksGrid from "./links"
 const Main = () => {
     return (
         <section className="rounded-2xl min-h-screen">
-            <div className="">
-                <div className="flex flex-col items-center pt-10">
-                    <Image className="mb-3 rounded-full shadow-lg border-2 p-1 border-azul-claro" src={Perfil} alt="Lucas Rodrigues" width={100} height={100} />
-                    <h5 className="mb-1 text-xl font-medium text-white">Lucas Rodrigues</h5>
-                    <span className="text-sm mb-6 text-gray-400">Desenvolvedor Front-end</span>
-                </div>
-                <div className="flex flex-col items-center space-y-4">
-                    <LinksGrid />
-                </div>
+            <div className="flex flex-col items-center pt-24">
+                <Image className="mb-3 rounded-full shadow-lg border-2 p-1 border-azul-claro" src={Perfil} alt="Lucas Rodrigues" width={100} height={100} />
+                <h5 className="mb-1 text-xl font-medium text-white">Lucas Rodrigues</h5>
+                <span className="text-sm font-semibold mb-6 text-gray-300">Desenvolvedor Front-end</span>
+            </div>
+            <div className="flex flex-col items-center space-y-4">
+                <LinksGrid />
             </div>
         </section>
     )
