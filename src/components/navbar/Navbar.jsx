817b467bde1bbcaa0069a8ca4c394355cbd1ddae
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-02-23 19:10:51 -0300

    ✨ Always visible menu button

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -24,10 +24,10 @@ const Navbar = () => {
 
     return (
         <nav className="flex w-full justify-center fixed">
-            <div className={`-ml-4 mx-4 flex w-full lg:w-2/5 border-2 border-zinc-900 bg-gray-50/70 items-center justify-between px-4 py-2 rounded-full backdrop-blur-sm transition-opacity duration-500 ease-in-out ${isMenuVisible ? '' : 'opacity-0'}`}>
+            <div className={`-ml-4 mx-4 flex w-full lg:w-2/5 border-2 border-zinc-900 bg-gray-50/70 items-center justify-between px-4 py-2 rounded-full backdrop-blur-sm transition-visible duration-500 ease-in-out ${isMenuVisible ? '' : 'invisible'}`}>
                 <Image className="rounded-xl border-2 border-zinc-900" src={Perfil} alt="Lucas Rodrigues" width={50} height={50} />
                 <h5 className="mb-1 text-xl font-semibold text-zinc-800">Lucas Rodrigues</h5>
-                <div className="bg-zinc-200 hover:bg-zinc-400 hover:text-zinc-200 p-2 rounded-full">
+                <div className="visible bg-zinc-200 hover:bg-zinc-400 hover:text-zinc-200 p-2 rounded-full">
                     <MoreHorizontal size={18} />
                 </div>
             </div>
