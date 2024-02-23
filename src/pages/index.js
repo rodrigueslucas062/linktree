2308fcb59df7f1cadc4bd56b123b64b488646ee1
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-02-23 16:53:40 -0300

    ✨ Navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import Main from "@/components/main/Main";
+import Navbar from "@/components/navbar/Navbar";
 
 export default function Home() {
   return (
-    <main className="zig-zag min-h-screen min-w-screen p-4">
-      <Main />
-    </main>
+    <>
+      <main className="zig-zag min-h-screen min-w-screen p-4">
+        <Navbar />
+        <Main />
+      </main>
+    </>
   );
 }
