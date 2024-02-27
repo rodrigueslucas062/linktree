c5a8b507f95b361cfdf166230ccb40106efd23c4
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-02-26 23:58:37 -0300

    ✨🚧 Modals and sharing system

--- src/components/main/links.js
+++ src/components/main/links.js
@@ -1,5 +1,5 @@
-import { FileCode, Github, Linkedin, MoreHorizontal } from "lucide-react";
-import Link from "next/link";
+import * as Dialog from "@radix-ui/react-dialog";
+import { ChevronRight, FileCode, Github, Linkedin, MessageCircle, MoreHorizontal, X } from "lucide-react";
 import { useState } from "react";
 
 const links = [
@@ -32,24 +32,53 @@ const Links = ({ link, titulo, icone }) => {
   const shadowStyle = {
     boxShadow: isHovered ? "4px 4px 0px rgba(0, 0, 0, 0.75)" : "8px 8px 0px rgba(0, 0, 0, 0.75)"
   };
-  
+
+  const handleLinkClick = (e) => {
+    if (!e.defaultPrevented) {
+      window.open(link, "_blank")
+    }
+  }
+
   return (
-    <Link
-      href={link}
-      className="rounded-lg cursor-pointer inline-block m-1 p-3 w-full lg:w-1/3 relative transition ease-in-out delay-75 bg-white border-4 border-zinc-900 text-zinc-900 hover:translate-y-1"
-      target="_blank"
-      style={shadowStyle}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <div className="flex items-center justify-between group">
-        {Icone && <Icone />}
-        <span className="font-semibold">{titulo}</span>
-        <div className="lg:invisible group-hover:visible bg-white hover:bg-gray-200 p-2 rounded-full">
-          <MoreHorizontal size={18} />
+    <Dialog.Root>
+      <div
+        onClick={handleLinkClick}
+        className="rounded-lg cursor-pointer inline-block m-1 p-3 w-full lg:w-1/3 relative transition ease-in-out delay-75 bg-white border-4 border-zinc-900 text-zinc-900 hover:translate-y-1"
+        style={shadowStyle}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)} >
+        <div className="flex items-center justify-between group">
+          {Icone && <Icone />}
+          <span className="font-semibold">{titulo}</span>
+          <Dialog.Trigger
+            className="lg:invisible group-hover:visible bg-white hover:bg-gray-200 p-2 rounded-full"
+            onClick={(e) => e.stopPropagation()}>
+            <MoreHorizontal size={18} />
+          </Dialog.Trigger>
         </div>
       </div>
-    </Link>
+
+      <Dialog.Portal>
+        <Dialog.DialogOverlay className="inset-0 fixed bg-black/20">
+          <Dialog.DialogContent className="fixed z-10 inset-0 md:inset-auto max-md:top-1/2 lg:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-gray-200 rounded-3xl flex flex-col outline-none overflow-hidden">
+            <Dialog.Close className="hover:bg-gray-300 p-2 rounded-full absolute top-2 right-2">
+              <X className="size-5" />
+            </Dialog.Close>
+            <div className="flex items-center justify-center gap-3 p-5">
+              <div className="rounded-lg mt-8 inline-block m-1 p-3 w-5/6 relative bg-white border-4 border-zinc-900 text-zinc-900">
+                <div className="flex items-center justify-between">
+                  <MessageCircle />
+                  <span>Compartilhar no WhatsApp</span>
+                  <div className="hover:bg-gray-300 rounded-full p-2">
+                    <ChevronRight />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Dialog.DialogContent>
+        </Dialog.DialogOverlay>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 };
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
+import * as Dialog from "@radix-ui/react-dialog";
 import Image from "next/image"
 import Perfil from '../../../public/images/perfil.jpeg'
-import { MoreHorizontal } from "lucide-react"
+import { ChevronRight, MessageCircle, MoreHorizontal, X } from "lucide-react"
 import { useEffect, useState } from "react"
 
 const Navbar = () => {
@@ -27,9 +28,33 @@ const Navbar = () => {
             <div className={`-ml-4 mx-4 flex w-full lg:w-2/5 border-2 border-zinc-900 bg-gray-50/70 items-center justify-between px-4 py-2 rounded-full backdrop-blur-sm transition-visible duration-500 ease-in-out ${isMenuVisible ? '' : 'invisible'}`}>
                 <Image className="rounded-xl border-2 border-zinc-900" src={Perfil} alt="Lucas Rodrigues" width={50} height={50} />
                 <h5 className="mb-1 text-xl font-semibold text-zinc-800">Lucas Rodrigues</h5>
-                <div className="visible bg-zinc-200 hover:bg-zinc-400 hover:text-zinc-200 p-2 rounded-full">
-                    <MoreHorizontal size={18} />
-                </div>
+                <Dialog.Root>
+                    <Dialog.Trigger className="visible bg-zinc-200 hover:bg-zinc-400 hover:text-zinc-200 p-2 rounded-full">
+                        <MoreHorizontal size={18} />
+                    </Dialog.Trigger>
+
+                    <Dialog.Portal>
+                        <Dialog.DialogOverlay className="inset-0 fixed bg-black/20">
+                            <Dialog.DialogContent className="fixed z-10 inset-0 md:inset-auto max-md:top-1/2 lg:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-gray-200 rounded-3xl flex flex-col outline-none overflow-hidden">
+                                <Dialog.Close className="hover:bg-gray-300 p-2 rounded-full absolute top-2 right-2">
+                                    <X className="size-5" />
+                                </Dialog.Close>
+                                <div className="flex items-center justify-center gap-3 p-5">
+                                    <div className="rounded-lg mt-8 inline-block m-1 p-3 w-5/6 relative bg-white border-4 border-zinc-900 text-zinc-900">
+                                        <div className="flex items-center justify-between">
+                                            <MessageCircle />
+                                            <span>Compartilhar no WhatsApp</span>
+                                            <div className="hover:bg-gray-300 rounded-full p-2">
+                                                <ChevronRight />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Dialog.DialogContent>
+                        </Dialog.DialogOverlay>
+                    </Dialog.Portal>
+
+                </Dialog.Root>
             </div>
         </nav>
     );
